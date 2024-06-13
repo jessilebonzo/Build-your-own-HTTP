41082885342babe466a6bb583c81c29104b6dd94
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 17:23:03 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,19 +1,23 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
+  
+  socket.on("data", (data) => {
+    const request = data.toString();
+    if (request.startsWith('GET /')) {
+      const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
+          socket.write(httpResponse);
+      } else {
+        const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
+          socket.write(httpResponse);
+    }
     socket.end();
-    server.close();
+    
   });
 });
-// Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-//
-server.listen(4221, "localhost");
+
+server.listen(4221, "localhost", () => {
+  process.stdout.write("Listening on localhost:4221");
+});
+
