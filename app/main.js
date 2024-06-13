ff7db79c7ec158d315619a0e4cbae628b95e7692
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 20:32:17 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,39 +1,40 @@
 const fs = require("fs");
 const net = require("net");
 
-
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
 
   socket.on("close", () => {
     socket.end();
-    
   });
 
   socket.on("data", (data) => {
     const req = data.toString();
+    console.log(req);
     const [firstLine, ...headers] = req.split("\r\n");
     const [method, path] = firstLine.split(" ");
 
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path.startsWith("/files/") && method === "GET") {
+    } else if (path.startsWith("/files/")) {
       const directory = process.argv[2];
       const filename = path.split("/files/")[1];
       const filePath = `${directory}/${filename}`;
-      if (fs.existsSync(filePath)) {
-        const content = fs.readFileSync(filePath);
-        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
-        socket.write(res);
-      } else {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      
+      if (method === "GET") {
+        if (fs.existsSync(filePath)) {
+          const content = fs.readFileSync(filePath);
+          const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+          socket.write(res);
+        } else {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+      } else if (method === "POST") {
+        const body = req.split("\r\n\r\n")[1];
+        fs.writeFileSync(filePath, body);
+        socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
       }
-    } else if (path.startsWith("/files/") && method === "POST") {
-      const filename = process.argv[2] + "/" + path.split("/files/")[1];
-      const body = req.split("\r\n\r\n")[1];
-      fs.writeFileSync(filename, body);
-      socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
     } else if (path === "/user-agent") {
       const userAgentHeader = headers.find(header => header.startsWith("User-Agent:"));
       if (userAgentHeader) {
