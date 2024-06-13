5eb3ff42d934c6d2a5181e58574c5235974358fa
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 20:22:50 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
-const net = require("net");
 const fs = require("fs");
+const net = require("net");
+
 
 console.log("Logs from your program will appear here!");
 
@@ -7,44 +8,40 @@ const server = net.createServer((socket) => {
 
   socket.on("close", () => {
     socket.end();
-    server.close();
+
   });
 
   socket.on("data", (data) => {
-    const request = data.toString();
-    const [firstLine, ...headers] = request.split('\r\n');
-    const path = firstLine.split(" ")[1];
-
-    if (path === '/') {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } 
-    else if (path.includes("/echo/")) {
-      const content = path.split('/echo/')[1];
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
-    } 
-    else if (path === '/user-agent') {
-      const userAgentHeader = headers.find(header => header.startsWith('User-Agent:'));
-      if (userAgentHeader) {
-        const userAgent = userAgentHeader.split('User-Agent: ')[1];
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-      } else {
-        socket.write("HTTP/1.1 400 Bad Request\r\n\r\nNo User-Agent header found");
-      }
-    } 
+    const req = data.toString();
+    console.log(req);
+    const path = req.split(" ")[1];
+    if (path === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
     else if (path.startsWith("/files/")) {
       const directory = process.argv[3];
       const filename = path.split("/files/")[1];
       if (fs.existsSync(`${directory}/${filename}`)) {
         const content = fs.readFileSync(`${directory}/${filename}`).toString();
-        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
         socket.write(res);
       } else {
-        socket.write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n");
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
-    } 
-    else {
-      socket.write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n");
-    }
+    } else if (path === "/user-agent") {
+      req.split("\r\n").forEach((line) => {
+        if (line.includes("User-Agent")) {
+          const res = line.split(" ")[1];
+          socket.write(
+            `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n`
+          );
+        }
+      });
+    } else if (path.startsWith("/echo/")) {
+      const res = path.split("/echo/")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n\r`
+      );
+    } else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    socket.end();
   });
 });
 
