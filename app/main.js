a270012bdd1ff9424a02259d72974645fd6d4b08
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 19:57:37 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -10,9 +10,16 @@ const server = net.createServer((socket) => {
 
   socket.on("data", (data) => {
     const path = data.toString().split(" ")[1];
-    const responseStatus = path === '/' ? "200 OK" : "404 Not Found";
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
-  })
+    
+    if (path === '/') {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.includes("/echo/")) {
+      const content = path.split('/echo/')[1];
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
 });
 
 server.listen(4221, "localhost");
