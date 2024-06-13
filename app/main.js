1bce9afa1ab32dc013add76c6f331d674bf69ca8
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 17:26:26 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,23 +1,40 @@
 const net = require("net");
 
-
 const server = net.createServer((socket) => {
-  
   socket.on("data", (data) => {
     const request = data.toString();
-    if (request.startsWith('GET /')) {
-      const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
-          socket.write(httpResponse);
-      } else {
-        const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
-          socket.write(httpResponse);
+    const [requestLine] = request.split('\r\n');
+    const [method, path] = requestLine.split(' ');
+
+    if (method === 'GET' && path === '/') {
+      const httpResponse = 
+        'HTTP/1.1 200 OK\r\n' +
+        'Content-Type: text/plain\r\n' +
+        'Content-Length: 13\r\n' +
+        '\r\n' +
+        'Hello, world!';
+      socket.write(httpResponse);
+    } else {
+      const httpResponse = 
+        'HTTP/1.1 404 NOT FOUND\r\n' +
+        'Content-Type: text/plain\r\n' +
+        'Content-Length: 9\r\n' +
+        '\r\n' +
+        'Not Found';
+      socket.write(httpResponse);
     }
     socket.end();
-    
+  });
+
+  socket.on("error", (err) => {
+    console.error(`Socket error: ${err.message}`);
   });
 });
 
 server.listen(4221, "localhost", () => {
-  process.stdout.write("Listening on localhost:4221");
+  console.log("Listening on localhost:4221");
 });
 
+server.on("error", (err) => {
+  console.error(`Server error: ${err.message}`);
+});
