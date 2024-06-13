ee12ef6f999c2ba84fc3770020f70d04e55178d8
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 20:03:57 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,8 +1,9 @@
 const net = require("net");
+const headers = request.split(' ')[1];
 
 console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
-  // socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
   socket.on("close", () => {
     socket.end();
     server.close();
@@ -16,6 +17,9 @@ const server = net.createServer((socket) => {
     } else if (path.includes("/echo/")) {
       const content = path.split('/echo/')[1];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+    }else if(url == "/user-agent"){
+      const userAgent = headers[2].split('User-Agent: ')[1];
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
