108cb8ad263eb6b086f9c336fd4462916304ee38
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 20:07:01 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 const net = require("net");
-const headers = request.split(' ')[1];
+
 
 console.log("Logs from your program will appear here!");
+
 const server = net.createServer((socket) => {
 
   socket.on("close", () => {
@@ -10,16 +11,23 @@ const server = net.createServer((socket) => {
   });
 
   socket.on("data", (data) => {
-    const path = data.toString().split(" ")[1];
-    
+    const request = data.toString();
+    const [firstLine, ...headers] = request.split('\r\n');
+    const path = firstLine.split(" ")[1];
+
     if (path === '/') {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (path.includes("/echo/")) {
       const content = path.split('/echo/')[1];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
-    }else if(url == "/user-agent"){
-      const userAgent = headers[2].split('User-Agent: ')[1];
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+    } else if (path === '/user-agent') {
+      const userAgentHeader = headers.find(header => header.startsWith('User-Agent:'));
+      if (userAgentHeader) {
+        const userAgent = userAgentHeader.split('User-Agent: ')[1];
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+      } else {
+        socket.write("HTTP/1.1 400 Bad Request\r\n\r\nNo User-Agent header found");
+      }
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
